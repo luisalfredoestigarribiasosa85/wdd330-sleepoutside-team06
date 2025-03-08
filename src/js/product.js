78de8b4c968d058ac5e608a000cc23f546870d5a
pedromoo <moo23001@byui.edu>
2025-03-08 11:46:58 -0600

    Changed products.js added cheker for empty and an array cItems

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cItems = getLocalStorage("so-cart") || [];
+  cItems.push(product);
+  setLocalStorage("so-cart", cItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
