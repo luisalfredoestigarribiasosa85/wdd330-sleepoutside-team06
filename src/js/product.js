6e37550aa306c6215345d628f61115156a2e1822
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-03-07 23:01:08 -0300

    add item to cart assignment

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartProducts = getLocalStorage("so-cart") || [];
+  cartProducts.push(product);
+  setLocalStorage("so-cart", cartProducts);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
