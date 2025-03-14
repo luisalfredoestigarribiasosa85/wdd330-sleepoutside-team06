baea06b4057c23d7795f8484c5a4804e1ff6e619
smarquis20 <shaunmarquis@gmail.com>
2025-03-14 16:14:10 -0500

    Update to TeamAcitivity 2

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,20 +1,25 @@
 export default class ProductDetails {
-    constructor(category) {
-      this.category = category;
-      this.path = `../json/${this.category}.json`;
-    }
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
 
-    init(){
+  async init() {
+    // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    // the product details are needed before rendering the HTML
+    // once the HTML is rendered, add a listener to the Add to Cart button
+    // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
+    document.getElementById('addToCart')
+      .addEventListener('click', this.addToCart.bind(this));
+  }
 
-    }
-}
+  addProductToCart(product) {
+    setLocalStorage("so-cart", this.product);
+  }
 
-export function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+  renderProductDetails() {
 
-export function renderProductDetails() {
+  }
 
 }
