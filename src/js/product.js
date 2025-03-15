3cf8decd076284aac7fd20780cec9d7fc5cdfce4
smarquis20 <shaunmarquis@gmail.com>
2025-03-14 21:36:55 -0500

    update TeamActivity2 fixing lint errors

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,7 @@
-import { getLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
 
 // add to cart button event handler
 async function addToCartHandler(e) {
