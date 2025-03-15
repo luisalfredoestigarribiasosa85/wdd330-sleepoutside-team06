429be08aa844a38d4542560f9a5d91e092f50fb6
smarquis20 <shaunmarquis@gmail.com>
2025-03-14 21:42:36 -0500

    Refractor Product.js TeamActivity2

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,10 @@
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
